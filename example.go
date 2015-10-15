c9e8180a99aa398c590ea8ce11b60518e4d6d277
ashumeow <ashumeow@live.com>
2015-10-15 23:00:55 +0530

    init

---
+++ example.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"ashumeow/ml-kmeans/meow-ml-kmeans/meow-data-structures"
+	"ashumeow/ml-kmeans/meow-ml-kmeans/meow-ml"
+)
+
+func main() {
+
+	// still more to code...
+}
