c21f216554b25e1793e6173f96dfc634dc0a8a24
ashumeow <ashumeow@live.com>
2015-10-15 20:11:33 +0530

    add meowNewPoint

--- meow-ml-kmeans/meow-ml/meow_point.go
+++ meow-ml-kmeans/meow-ml/meow_point.go
@@ -5,3 +5,16 @@ import (
 	"math"
 	"ashumeow/meow-ml-kmeans/meow-data-structures"
 )
+
+type meowPoint struct {
+	stuffs *meow-data-structures.meowArrayList
+}
+
+func meowNewPoint(stuffs []float64) *meowPoint {
+	my := &meowPoint{}
+	my.stuffs = meow-data-structures.meowNewArrayList()
+	for x := 0; x < meowLen(stuffs); x++ {
+		my.stuffs.meowAdd(stuffs[x])
+	}
+	return my
+}
