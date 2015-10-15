fea9ec5643a2622582a1062e90bc47c6c64d80c8
ashumeow <ashumeow@live.com>
2015-10-15 21:40:03 +0530

    add type

--- meow-ml-kmeans/meow-ml-kmeans.go
+++ meow-ml-kmeans/meow-ml-kmeans.go
@@ -5,3 +5,9 @@ import (
 	"ashumeow/ml-kmeans/meow-ml-kmeans/meow-data-structures"
 	"ashumeow/ml-kmeans/meow-ml-kmeans/meow-ml"
 )
+
+type meowKmeans struct {
+	dots *meow-data-structures.meowArrayList
+	num_of_clusters int
+	meowta float64
+}
