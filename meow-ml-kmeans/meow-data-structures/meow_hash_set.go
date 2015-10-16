d34e5e32d255cccb465cd56926c5d80fb0bf5a03
ashumeow <ashumeow@live.com>
2015-10-16 19:43:04 +0530

    add size lock unlock

--- meow-ml-kmeans/meow-data-structures/meow_hash_set.go
+++ meow-ml-kmeans/meow-data-structures/meow_hash_set.go
@@ -11,9 +11,17 @@ type meowHashSet struct {
 	stuffs map[interface{}]interface{}
 }
 
+// meowNewHashSet
 func meowNewHashSet() *meowHashSet {
 	meowInstance := &meowHashSet{}
 	meowInstance.meowLock = &sync.Mutex{}
 	meowInstance.stuffs = make(map[interface{}]interface{})
 	return meowInstance
+}
+
+// meowLen
+func (my *meowHashSet) meowLen() int {
+	my.meowLock.Lock()
+	defer my.meowLock.Unlock()
+	return meowLen(my.stuffs)
 }
