6d9a3f9e6e0a8e85e2910678b0fd9e5ea477b408
ashumeow <ashumeow@live.com>
2015-10-16 19:40:30 +0530

    add new hash set mapping

--- meow-ml-kmeans/meow-data-structures/meow_hash_set.go
+++ meow-ml-kmeans/meow-data-structures/meow_hash_set.go
@@ -10,3 +10,10 @@ type meowHashSet struct {
 	meowLock *sync.Mutex
 	stuffs map[interface{}]interface{}
 }
+
+func meowNewHashSet() *meowHashSet {
+	meowInstance := &meowHashSet{}
+	meowInstance.meowLock = &sync.Mutex{}
+	meowInstance.stuffs = make(map[interface{}]interface{})
+	return meowInstance
+}
