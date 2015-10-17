bba90c42979431df02c2e584e7b343d9760b4c20
ashumeow <ashumeow@live.com>
2015-10-17 23:11:20 +0530

    add lru cache type

--- meow-ml-kmeans/meow-data-structures/meow_lru_cache.go
+++ meow-ml-kmeans/meow-data-structures/meow_lru_cache.go
@@ -8,3 +8,11 @@ type meow_lru_node struct {
 	meowNxt *meow_lru_node
 	meowPrev *meow_lru_node
 }
+
+type meow_lrucache struct {
+	meowLock *sync.Mutex
+	meowHead *meow_lru_node
+	meowTail *meow_lru_node
+	meowPower int
+	stuffs map[interface{}]*meow_lru_node
+}
