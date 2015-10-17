c2f4708623a5933d253c47a74804ad2752115609
ashumeow <ashumeow@live.com>
2015-10-17 22:50:40 +0530

    add meowPush

--- meow-ml-kmeans/meow-data-structures/meow_stack.go
+++ meow-ml-kmeans/meow-data-structures/meow_stack.go
@@ -18,3 +18,22 @@ func meowNewStack() *meowStack {
 	s.meowLock = &sync.Mutex{}
 	return s
 }
+
+func (s *meowStack) meowLen() int {
+	s.meowLock.Lock()
+	defer s.meowLock.Unlock()
+	return s.meowCount
+}
+
+func (s *meowStack) meowPush(stuffy interface{}) {
+	s.meowLock.Lock()
+	defer s.meowLock.Unlock()
+	ig := &meowStackNode { meowData: stuffy }
+	if s.meowHead == nil {
+		s.meowHead = ig
+	} else {
+		ig.meowNxt = s.meowHead
+		s.meowHead = ig
+	}
+	s.meowCount++
+}
