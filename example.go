810612d9a896fdbda0dcc3a3823c5237b3ba17d6
ashumeow <ashumeow@live.com>
2015-10-18 18:37:17 +0530

    init example _ WIP

--- example.go
+++ example.go
@@ -8,5 +8,28 @@ import (
 )
 
 func main() {
+	fmt.Println("---Queue---")
+	meowQueue := meow-data-structures.meowNewQueue()
+	meowQueue.meowPush("Testing... ")
+	meowQueue.meowPush(4)
+	meowQueue.meowPush(5)
+	meowQueue.meowPush(8)
+	meowQueue.meowPush(6)
+	meowQueue.meowPush("Hold on!")
+	fmt.Println("Total stuffs before distribution: ", meowQueue.meowLen())
+	fmt.Println("Power before distribution: ", meowQueue.meowErect())
+	fmt.Println(meowQueue.meowDistro(), 
+				meowQueue.meowDistro(), 
+				meowQueue.meowDistro(), 
+				meowQueue.meowDistro(), 
+				meowQueue.meowDistro(), 
+				meowQueue.meowDistro(), 
+				meowQueue.meowDistro()
+				)
+	fmt.Println("Total stuffs after distribution: ", meowQueue.meowLen())
+	fmt.Println("Power: ", meowQueue.meowErect())
+
+	fmt.Println("\n")
+
 	// still more to code...
 }
