9b6f970c07930a629e028694faab68425e05d8d5
ashumeow <ashumeow@live.com>
2015-10-18 17:59:12 +0530

    update init

--- example.go
+++ example.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"ashumeow/ml-kmeans/meow-ml-kmeans/meow-data-structures"
-	"ashumeow/ml-kmeans/meow-ml-kmeans/meow-ml"
-	"ashumeow/ml-kmeans/meow-ml-kmeans/"
+	"ashumeow/meow-ml-kmeans/meow-data-structures"
+	"ashumeow/meow-ml-kmeans/meow-ml"
+	"ashumeow/meow-ml-kmeans"
 )
 
 func main() {
-
-	// TODO : Finish meow-data-structures package before coding this one...
-	
 	// still more to code...
 }
